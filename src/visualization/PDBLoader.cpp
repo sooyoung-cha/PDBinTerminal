84bc58a999b10de252273d748d4c7117fc079184
LunaJang <lunajang@kakao.com>
2024-09-02 15:14:19 +0900

    change main

--- src/visualization/PDBLoader.cpp
+++ src/visualization/PDBLoader.cpp
@@ -1,9 +1,21 @@
 #include "PDBLoader.hpp"
 
 Protein load_data(string in_file){
-    Protein data;
-    ifstream openFile(in_file.data()); 
+    int len = 0;
 
+    ifstream openFile_len(in_file.data()); 
+    if(openFile.is_open()){
+        string line;
+        while(getline(openFile, line)){
+            if(line.substr(0, 4) == "ATOM" && line.substr(13, 2) == "CA"){
+                len += 1;
+            }
+        }
+    }     
+    openFile_len.close();
+
+    Protein data(len);
+    ifstream openFile_data(in_file.data()); 
     if(openFile.is_open()){
         string line;
         while(getline(openFile, line)){
@@ -11,6 +23,8 @@ Protein load_data(string in_file){
                 data.add_init_atoms(stof(line.substr(30, 8)), stof(line.substr(38, 8)), stof(line.substr(46, 8)));
             }
         }
-    }               
+    }      
+    openFile_data.close();
+
     return data;
 }
